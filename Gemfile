4be9d06bb7331ee1356cacd36f0fc078f1a15f5e
Maher Hawash <gmhawash@gmail.com>
2014-10-31 06:15:56 -0700

    Limit list of organizations to owned orgs.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development do
   gem 'rb-fchange', :require=>false
   gem 'rb-fsevent', :require=>false
   gem 'rb-inotify', :require=>false
+  gem 'pry-byebug'
 end
 group :development, :test do
   gem 'factory_girl_rails'
--- app/lib/github_user.rb
+++ app/lib/github_user.rb
@@ -1,9 +1,16 @@
 class GithubUser < Octokit::Client
   def owned_organizations
     organizations.map do |org|
-      teams = organization("%s/teams" % org.login)
-      next unless teams.one?{|t| t.name == 'Owners'}
-      org
+      # Github API does not provide a way to find if a person is an owner of an org,
+      # hence, this hack.  Try to update the org with a non-destructive update, and
+      # if success, then an owner, if 404 then not.
+      # http://stackoverflow.com/questions/20144295/github-api-v3-determine-if-user-is-an-owner-of-an-organization
+
+      begin
+        response = patch Octokit::Organization.path(org.login), {}
+        org
+      rescue Octokit::NotFound
+      end
     end.compact
   end
 
